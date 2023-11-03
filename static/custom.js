ab025a1d1654f316bf7638675d8674ceec7d510a
Eric Weinberg <ericweinberg@ToB106PQY19QG0H.localdomain>
2023-11-03 01:17:07 -0500

    ui work

--- static/custom.js
+++ static/custom.js
@@ -5,12 +5,6 @@ $(document).ready(function () {
     // Initialize lists to store user-generated restrictions and blocked times
     let userBlockedTimes = [];
 
-    // Initialize Tagify for the "Restrictions" field
-    const restrictionsField = document.querySelector('.tagify'); // Replace with the correct selector
-    const tagify = new Tagify(restrictionsField, {
-        // Configure Tagify as needed
-    });
-
     // Initialize drag-and-drop for rows using dragula
     const rowContainer = document.querySelector('#class-table-body'); // Replace with your table body's ID
     const rowDragula = dragula([rowContainer]);
@@ -19,36 +13,34 @@ $(document).ready(function () {
         const sectionTitle = el.dataset.sectionTitle;
 
         // Handle the drop event here
-        // Example: You can add a tag to the "Restrictions" field using the Tagify instance
+        // Example: You can add a tag to the "Restrictions" field
+        const restrictionsField = $(el).find('.tagify')[0];
+        const tagify = new Tagify(restrictionsField, {
+            // Configure Tagify as needed
+        });
         tagify.addTags([sectionTitle]);
 
         // Update user-generated restrictions
         userBlockedTimes.push(sectionTitle);
     });
 
-    // Handle the selection of blocked times
-    $('#timeslot-list .ui-selected').on('click', function () {
-        const selectedTime = $(this).text();
+    // Initialize Select2 for the multi-select dropdowns with the .blocked-times-select class
+    $('.blocked-times-select').select2({ width: '300px' });
 
-        // Toggle the selection and update the UI
-        if ($(this).hasClass('blocked-time')) {
-            $(this).removeClass('blocked-time');
-            // Remove the time from the user-selected blocked times
-            userBlockedTimes = userBlockedTimes.filter(time => time !== selectedTime);
-        } else {
-            $(this).addClass('blocked-time');
-            // Add the time to the user-selected blocked times
-            userBlockedTimes.push(selectedTime);
-        }
+    // Handle changes in any of the multi-select dropdowns
+    $('.blocked-times-select').on('change', function () {
+        const selectedTimeslots = $(this).val();
+
+        // Update user-selected blocked times
+        userBlockedTimes = selectedTimeslots || [];
+
+        // Log the selected timeslots (you can remove this)
+        console.log('Selected Timeslots:', userBlockedTimes);
     });
+
 
     // Handle the "Apply Constraints & Optimize" button click event
     $('#apply-constraints').on('click', function () {
-        // Collect the selected timeslots
-        const selectedTimeslots = $('#timeslot-list .ui-selected').map(function () {
-            return $(this).text();
-        }).get();
-
         // Send the user-selected blocked times to the server for processing
         const requestData = {
             userBlockedTimes: userBlockedTimes,
